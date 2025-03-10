c0b230e10dbeefbf1234eb0246dabd6fc5a4093a
bitchikogulara <bichiko2004@gmail.com>
2025-03-10 20:31:35 +0100

    Initial Commit

---
+++ PersonalLibraryManager/ContentView.swift
@@ -0,0 +1,21 @@
+//
+//  ContentView.swift
+//  PersonalLibraryManager
+//
+//  Created by mac on 10.03.25.
+//
+
+import SwiftUI
+
+struct ContentView: View {
+    var body: some View {
+        Text("Hello, world!")
+            .padding()
+    }
+}
+
+struct ContentView_Previews: PreviewProvider {
+    static var previews: some View {
+        ContentView()
+    }
+}
---
+++ PersonalLibraryManager/PersonalLibraryManagerApp.swift
@@ -0,0 +1,17 @@
+//
+//  PersonalLibraryManagerApp.swift
+//  PersonalLibraryManager
+//
+//  Created by mac on 10.03.25.
+//
+
+import SwiftUI
+
+@main
+struct PersonalLibraryManagerApp: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
